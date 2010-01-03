60b91359fdd65ae6078892f5eb48ef88826da76e
Robert Sesek <rsesek@bluestatic.org>
2010-01-03 10:05:56 -0800

    Add forgotten form_key_event_test.php

---
+++ testing/tests/input/form_key_event_test.php
@@ -0,0 +1,67 @@
+<?php
+// Phalanx
+// Copyright (c) 2009 Blue Static
+// 
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or any later version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace phalanx\test;
+use \phalanx\input as input;
+
+require_once 'PHPUnit/Framework.php';
+
+require_once TEST_ROOT . '/tests/input.php';
+
+class FormKeyEventTest extends \PHPUnit_Framework_TestCase
+{
+    public $event;
+    public $pump;
+    public $form_key;
+
+    public function setUp()
+    {
+        $this->pump = new \phalanx\events\EventPump();
+        $this->form_key = new input\FormKeyManager(new TestFormKeyManagerDelegate());
+        $this->event = new input\ValidateFormKeyEvent($this->form_key);
+    }
+
+    public function testCtor()
+    {
+        $this->assertAttributeSame($this->form_key, 'manager', $this->event);
+    }
+
+    public function testNoInput()
+    {
+        $this->pump->PostEvent($this->event);
+        $this->assertTrue($this->event->is_cancelled());
+    }
+
+    public function testGETKey()
+    {
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $_GET['phalanx_form_key'] = 'foo';
+        $this->pump->PostEvent($this->event);
+        $this->assertTrue($this->event->is_cancelled());
+    }
+
+    public function testInvalidPOST()
+    {
+        $_SERVER['REQUEST_METHOD'] = 'POST';
+        $_POST['phalanx_form_key'] = 'foo';
+
+        $this->setExpectedException('phalanx\input\FormKeyException');
+        $this->pump->PostEvent($this->event);
+
+        $this->assertFalse($this->event->is_cancelled());
+        $this->assertTrue($this->form_key->delegate()->did_get);
+    }
+}
